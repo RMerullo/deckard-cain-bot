b2b4438d21a8a1a870ffa4403763eb9969f4c405
Russell Merullo <rmerullo1@gmail.com>
2026-03-13 21:18:28 -0400

    Added runeword data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { HomeScreen } from "../homescreen/homescreen";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Deckard Cain Bot" },
+    { name: "description", content: "Diablo 2 Helper" },
   ];
 }
 
